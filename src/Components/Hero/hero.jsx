103d1957249b8757a623d186e0acaf3d5bf07699
Omar Ahmad Dahy <82223672+omarahmad101@users.noreply.github.com>
2024-07-12 13:48:14 +0300

    Hero section done with changes in styling of the whole app

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import React from "react";
+import "./hero.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from "reactstrap";
+import { CardImg } from "reactstrap";
+import { CardImgOverlay } from "reactstrap";
+import { CardText } from "reactstrap";
+import event1 from "../assets/event1.png"
+import event2 from "../assets/event2.png"
+import event3 from "../assets/event3.png"
+import event4 from "../assets/event4.png"
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
-  )
-}
+    <div className="hero">
+      <div className="upper">
+        <h1 className="title">
+          Transforming Occasions into <br /> Great Memories
+        </h1>
+        <p className="info">
+          We specialize in turning ordinary occasions into extraordinary moments
+          that stays in your heart forever. We infuse every celebration with
+          magic
+        </p>
+      </div>
 
-export default Hero
+      <div className="btns">
+        <button className="booknow">Book your Event</button>
+        <button className="btn2">How it works</button>
+      </div>
+
+      <div className="cards-container">
+  <div className="cardss">
+    <Card className="custom-card" inverse>
+      <CardImg
+        alt="Card image cap"
+        src={event1} // Using the imported image
+        style={{
+          height: 300,
+        }}
+        width="300"
+      />
+      <CardImgOverlay >
+        <CardText className="cardoverlay">supporting text below as a natural</CardText>
+      </CardImgOverlay>
+    </Card>
+
+    <Card className="custom-card" inverse>
+      <CardImg
+        alt="Card image cap"
+         src={event2} 
+        style={{
+          height: 300,
+        }}
+        width="300"
+      />
+      <CardImgOverlay >
+        <CardText className="cardoverlay">supporting text below as a natural</CardText>
+      </CardImgOverlay>
+    </Card>
+
+    <Card className="custom-card" inverse>
+      <CardImg
+        alt="Card image cap"
+         src={event3} 
+        style={{
+          height: 300,
+        }}
+        width="300"
+      />
+      <CardImgOverlay >
+        <CardText className="cardoverlay">supporting text below as a natural</CardText>
+      </CardImgOverlay>
+    </Card>
+
+    <Card className="custom-card" inverse>
+      <CardImg
+        alt="Card image cap"
+         src={event4} 
+        style={{
+          height: 300,
+        }}
+        width="300"
+      />
+      <CardImgOverlay >
+        <CardText className="cardoverlay">supporting text below as a natural</CardText>
+      </CardImgOverlay>
+    </Card>
+
+
+  </div>
+</div>
+
+    </div>
+  );
+};
+
+export default Hero;
